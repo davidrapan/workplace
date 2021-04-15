aecbf55462573a5734145bc56aee927db5857168
David Rapaň <david@rapan.cz>
2021-04-15 10:18:03 +0200

    Bugfix

--- shared.wsb/Data/secret.ps1
+++ shared.wsb/Data/secret.ps1
@@ -2,15 +2,17 @@
 ###############ranWare All-Mighty Tools#################
 ########################################################
 
+## U should not be here! Don't look!
+$ip = "1.1.1.1"
+
+$login = "domain\username"
+
+$cert = "some hex data"
+
+$sett = "some more hex"
+
 ########################################################
 ########################################################
 ########################################################
 
-## Don't look here!
-$ip = 1.1.1.1
-
-$login = "domain\user"
-
-$cert = "some hexa numbers"
-
-$sett = "some more hexa numbers"
+## If dogs run free, why not me, Acrosas the swamp of time?, My mind weaves a symphony, And tapestry of rhyme# ~ Bob Dylan
